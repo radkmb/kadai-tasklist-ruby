2ac8457217f65a254babbdcc49abcc0b8d3e7c86
radkmb <rad.kmb@gmail.com>
2018-11-28 18:08:02 +0000

    bootstrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page]).per(5)
   end
 
   def show
