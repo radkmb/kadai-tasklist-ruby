58ef12f7387ab88a72cccefc6db9a66d29706834
radkmb <rad.kmb@gmail.com>
2018-11-28 00:27:42 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,6 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
