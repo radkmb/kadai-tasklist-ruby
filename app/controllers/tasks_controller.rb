77328fca41e554d44bb501919ba6beaa23cc8188
radkmb <rad.kmb@gmail.com>
2018-12-01 09:55:43 +0000

    Fix tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in
+  #before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
